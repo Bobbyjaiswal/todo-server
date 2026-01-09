8aa60403bddb313a27901ecaea4fd7004002311c
bobbyjaiswal <sachin.jaiswalgurukul@gmail.com>
2026-01-09 14:42:22 +0530

    Port Number Changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   return res.status(200).json("This Is Home Page")
