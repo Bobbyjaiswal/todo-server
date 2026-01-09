65349e90b5c5816ec63f91ecd7864ebe8ac92385
bobbyjaiswal <sachin.jaiswalgurukul@gmail.com>
2026-01-09 14:13:13 +0530

    Project Structure Updated

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import express from "express"
 
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  return res.status(200).json("This Is Home Page")
+
+})
+
+app.listen(port, () => {
+  console.log("Server Started")
+})
